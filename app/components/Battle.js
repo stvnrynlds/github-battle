78701962f5d7b3c33d50b1930fdb0df95ec3b1af
Steven Reynolds <steven@stevenreynolds.me>
2017-08-30 22:03:22 -0700

    Wrote reset function, wired reset btn

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -6,10 +6,10 @@ class Battle extends React.Component {
     super(props);
 
     this.state = {
-      playerOneName: '',
-      playerOneImage: '',
-      playerTwoName: '',
-      playerTwoImage: ''
+      playerOneName: null,
+      playerOneImage: null,
+      playerTwoName: null,
+      playerTwoImage: null
     }
 
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -25,7 +25,13 @@ class Battle extends React.Component {
     });
   }
 
-  handleReset() {
+  handleReset(id) {
+    this.setState(function() {
+      var newState = {};
+      newState[id + 'Name'] = null;
+      newState[id + 'Image'] = null;
+      return newState;
+    })
     console.log('handleReset fired');
   }
 
@@ -38,12 +44,13 @@ class Battle extends React.Component {
     return (
       <div>
         <div className="row">
+
           {!playerOneName &&
             <PlayerInput
               id="playerOne"
               label="Player One"
               onSubmit={this.handleSubmit} />}
-          {!playerOneImage !== null &&
+          {playerOneImage !== null &&
             <PlayerPreview
               avatar={playerOneImage}
               username={playerOneName}
@@ -57,7 +64,7 @@ class Battle extends React.Component {
               id="playerTwo"
               label="Player Two"
               onSubmit={this.handleSubmit} />}
-          {!playerTwoImage !== null &&
+          {playerTwoImage !== null &&
             <PlayerPreview
               avatar={playerTwoImage}
               username={playerTwoName}
@@ -75,15 +82,15 @@ class Battle extends React.Component {
 function PlayerPreview (props) {
   return (
     <div>
-      <div className="column">
+      <div className='column'>
         <img
-          className="avatar"
+          className='avatar'
           src={props.avatar}
           alt={'Avatar for ' + props.username } />
-        <h2 className="username">@{props.username}</h2>
+        <h2 className='username'>@{props.username}</h2>
       </div>
       <button
-        className="reset"
+        className='reset'
         onClick={props.onReset.bind(null, props.id)}
         >
         Reset
@@ -95,7 +102,7 @@ function PlayerPreview (props) {
 PlayerPreview.propTypes = {
   avatar: PropTypes.string.isRequired,
   username: PropTypes.string.isRequired,
-  // onReset: PropTypes.func.isRequired,
+  onReset: PropTypes.func.isRequired,
   id: PropTypes.string.isRequired
 }
 
@@ -124,17 +131,25 @@ class PlayerInput extends React.Component {
   }
   render() {
     return (
-      <form className="column" onSubmit={this.handleSubmit}>
-        <label className="header" htmlFor="username">{ this.props.label }</label>
-          <input id="username" placeholder="github username" type="text" autoComplete="off"
-            value={this.state.username}
-            onChange={this.handleChange}
-          />
+      <form
+        className='column'
+        onSubmit={this.handleSubmit}>
+        <label
+          className='header'
+          htmlFor='username'>
+            { this.props.label }
+        </label>
+        <input
+          id='username'
+          placeholder='github username'
+          type='text'
+          autoComplete='off'
+          value={this.state.username}
+          onChange={this.handleChange} />
         <button
-          className="button"
-          type="submit"
-          disabled={!this.state.username}
-          >
+          className='button'
+          type='submit'
+          disabled={!this.state.username}>
             Submit
           </button>
       </form>

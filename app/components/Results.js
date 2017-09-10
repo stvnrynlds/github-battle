aa11048dc1e95939a523fe8bcf35a56a8ea3fcb4
Steven Reynolds <steven@stevenreynolds.me>
2017-09-09 22:50:28 -0700

    @18:20, Composition via props.children

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,6 +1,30 @@
 var React = require('react');
+var PropTypes = require('prop-types');
 var queryString = require('query-string');
 var api = require('../utils/api');
+var Link = require('react-router-dom').Link;
+var PlayerPreview = require('./PlayerPreview');
+
+function Player (props) {
+  var user = props.profile;
+  return (
+    <div>
+    <div className='column'>
+      <img src={props.avatar} alt="" className="avatar"/>
+    </div>
+      <h2>{props.label}: {user.login}</h2>
+      <p></p>
+      <p>Score: {props.score}</p>
+    </div>
+  );
+}
+
+Player.propTypes = {
+  label: PropTypes.string.isRequired,
+  profile: PropTypes.object.isRequired,
+  score: PropTypes.number.isRequired
+
+}
 
 class Results extends React.Component {
   constructor(props) {
@@ -18,15 +42,59 @@ class Results extends React.Component {
       players.playerOneName,
       players.playerTwoName
     ]).then(function (results) {
-      console.log(results);
-    });
+      if (results === null) {
+        return this.setState(function() {
+          return {
+            error: 'Looks like there was an error. Check that both users exist on Github',
+            loading: false,
+          }
+        });
+      }
+
+      this.setState(function() {
+        return {
+          error: null,
+          winner: results[0],
+          loser: results[1],
+          loading: false
+        }
+
+      })
+    }.bind(this));
   }
   render() {
     var error = this.state.error;
     var winner = this.state.winner;
+    var loser = this.state.loser;
+    var loading = this.state.loading;
     
+    if (loading === true) {
+      return <p>Loading</p>
+    }
+
+    if (error) {
+      return (
+        <div>
+          <p>{error}</p>
+          <Link to='/battle'>Reset</Link>
+        </div>
+      )
+    }
+
     return (
-      <div>Results</div>
+      <div className='row'>
+        <Player
+          label='Winner'
+          score={winner.score}
+          profile={winner.profile}
+        />
+        <Player
+          label='Loser'
+          score={loser.score}
+          profile={loser.profile}
+        />
+        
+      </div>
     )
   }
 }
